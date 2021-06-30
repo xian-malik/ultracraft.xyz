2cd53d0bf03be4851774dc9ca6d968d8b41a175e
Malik <xian-malik@outlook.com>
2021-07-01 02:06:10 +0600

    Voting added

--- content/voteLinks.content.js
+++ content/voteLinks.content.js
@@ -13,6 +13,14 @@ module.exports = {
 			url: "https://topminecraftservers.org/vote/10177"
 		},
 		{
+			title: "Minecraft Server List",
+			url: "https://minecraft-server-list.com/server/478788/vote/"
+		},
+		{
+			title: "TOP G",
+			url: "https://topg.org/minecraft-servers/server-604527"
+		},
+		{
 			title: "Planet Minecraft",
 			url: "https://www.planetminecraft.com/server/ultracraft-4473894/vote/"
 		}
