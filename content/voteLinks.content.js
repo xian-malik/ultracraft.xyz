a75e362a537ad99ec637cf21ae4134410b071db1
Malik <xian-malik@outlook.com>
2021-07-01 16:59:16 +0600

    API test

--- content/voteLinks.content.js
+++ content/voteLinks.content.js
@@ -1,5 +1,4 @@
 module.exports = {
-	
 	links: [
 		{
 			title: "Minecraft Multiplayer",
--- pages/u/login.js
+++ pages/u/login.js
@@ -11,26 +11,47 @@ export default function Login() {
 
 	const onSubmit = e => {
 		e.preventDefault();
-		console.log( username, password );
+		let url = 'http://localhost:8443/auth/signin';
+
+		var data = {
+			username: username,
+			password: password
+		}
+		fetch(url, {
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json; charset=utf-8',
+				'Access-Control-Allow-Headers': '*'
+			},
+			method: "POST",
+			body: JSON.stringify(data)
+		})
+		.then( res => {
+			return res.json()
+		})
+		.then( res => {
+			console.log( res );
+		})
+		.catch(function(res){ console.log(res) });
 	}
 
 	return (
-		<main>
+		<main	>
 			<Head>
-				<title>Login Portal</title>
+				<title>Login Portal</title>	
 			</Head>
 			<div className="page-wrapper">
 				<Header />
-				<section className="h-screen w-screen grid items-center justify-center">
-					<article className="mx-auto bg-uc-secondary rounded-md p-8 block">
-						<h2 className="text-4xl text-center mb-16">Login</h2>
-						<form onSubmit={ onSubmit } className="grid grid-cols-5">
-							<input placeholder="Username" name="username"
+				<section className="h-screen w-screen grid items-center justify-center grid-cols-1">
+					<article className="mx-auto bg-uc-secondary rounded-md p-8 block w-full max-w-sm">
+						<h2 className="text-4xl text-center mb-12">Login</h2>
+						<form onSubmit={ onSubmit } className="grid grid-cols-1 w-full">
+							<input placeholder="Username" name="username" type="text"
 								value={ username } onChange={ e => setUsername(e.target.value) }
-								className="bg-transparent border-b border-uc-primary py-2 px-4 col-span-2" />
-							<input placeholder="Password" name="password"
+								className="bg-transparent border-b border-uc-primary py-2 px-4" />
+							<input placeholder="Password" name="password" type="password"
 								value={ password } onChange={ e => setPassword(e.target.value) }
-								className="bg-transparent border-b border-uc-primary mx-4 py-2 px-4 col-span-2" />
+								className="bg-transparent border-b border-uc-primary py-2 px-4 my-4" />
 							<input className="rounded-lg cursor-pointer bg-uc-primary text-uc-secondary py-2 px-8 uppercase w-full font-medium text-base tracking-wider" type="submit" value="Submit" />
 						</form>
 					</article>
