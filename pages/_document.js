563ea9a414d53b135b3dc68ec5c556443afb92cb
Malik <xian-malik@outlook.com>
2021-07-03 14:45:39 +0600

    Messenger Chat

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,34 @@ class MyDocument extends Document {
           
           <NextScript />
 
+          <div id="fb-root"></div>
+          <div id="fb-customer-chat" className="fb-customerchat"></div>
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                var chatbox = document.getElementById('fb-customer-chat');
+                chatbox.setAttribute("page_id", "118464276227049");
+                chatbox.setAttribute("attribution", "biz_inbox");
+
+                window.fbAsyncInit = function() {
+                  FB.init( {
+                    xfbml            : true,
+                    version          : 'v11.0'
+                  } );
+                };
+
+                (function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                  fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+              `,
+            }}
+          />
+
           {/* <img alt="Cursor" className="custom-cursor-sword" src="https://res.cloudinary.com/xianmalik/image/upload/w_50,h_50,q_auto,f_auto/v1589137162/UltraCraft/cursor.png" /> */}
         </body>
       </Html>
